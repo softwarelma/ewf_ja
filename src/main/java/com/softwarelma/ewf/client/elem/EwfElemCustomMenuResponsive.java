5ee14c29395fe00b0957d5205a69487d846686a7
ellison <ellison@pc-ellison>
2018-01-24 19:13:34 +0100

    VERTICAL MENU

--- src/main/java/com/softwarelma/ewf/client/elem/EwfElemCustomMenuResponsive.java
+++ src/main/java/com/softwarelma/ewf/client/elem/EwfElemCustomMenuResponsive.java
@@ -4,6 +4,7 @@ import com.softwarelma.epe.p1.app.EpeAppException;
 import com.softwarelma.epe.p1.app.EpeAppUtils;
 import com.softwarelma.ewf.client.EwfClient;
 import com.softwarelma.ewf.client.cont.EwfContentBean;
+import com.vaadin.ui.AbstractLayout;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
@@ -12,47 +13,68 @@ import com.vaadin.ui.VerticalLayout;
 
 public class EwfElemCustomMenuResponsive extends EwfElemAbstract {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public EwfElemCustomMenuResponsive(EwfClient client, UI ui, EwfElemBean elemBean, EwfContentBean contentBean)
-			throws EpeAppException {
-		super(client, ui, elemBean, contentBean);
-	}
+    public EwfElemCustomMenuResponsive(EwfClient client, UI ui, EwfElemBean elemBean, EwfContentBean contentBean)
+            throws EpeAppException {
+        super(client, ui, elemBean, contentBean);
+    }
 
-	@Override
-	public Component getComponent() throws EpeAppException {
-		if (super.getComponent() != null) {
-			return super.getComponent();
-		}
+    @Override
+    public Component getComponent() throws EpeAppException {
+        if (super.getComponent() != null) {
+            return super.getComponent();
+        }
 
-		EpeAppUtils.checkNull("mapPageAndDescription", getMapPageAndDescription());
+        EpeAppUtils.checkNull("mapPageAndDescription", getMapPageAndDescription());
 
-		// TODO
-		// 1. make a vert lay with both: hor and vert menu
-		// 2. add toggle classes
-		// 3. set scss ewf classes
-		// 4. add menu main button for vert
+        // TODO
+        // 1. make a vert lay with both: hor and vert menu
+        // 2. add toggle classes
+        // 3. set scss ewf classes
+        // 4. add menu main button for vert
 
-		// HorizontalLayout layout = new HorizontalLayout();
-		VerticalLayout layout = new VerticalLayout();
-		// layout.setWidth("100%");
+        VerticalLayout layout = new VerticalLayout();
+        // layout.setWidth("100%");
+        HorizontalLayout layoutHor = new HorizontalLayout();
+        VerticalLayout layoutVert = new VerticalLayout();
+        this.addButtons(layoutHor);
+        this.addButtonOpenClose(layoutVert);
+        layout.addComponent(layoutHor);
+        layout.addComponent(layoutVert);
+        return layout;
+    }
 
-		for (String pageName : getMapPageAndDescription().keySet()) {
-			Button button = new Button(getMapPageAndDescription().get(pageName));
-			button.addClickListener(e -> {
-				// getUi().getPage().setLocation("/" +
-				// EwfCommonConstants.APPLICATION_NAME + "/" + pageName +
-				// "/");
-				try {
-					getClient().loadPage(getUi(), pageName);
-				} catch (EpeAppException ex) {
-				}
-			});
-			layout.addComponent(button);
-			// layout.setComponentAlignment(button, Alignment.MIDDLE_LEFT);
-		}
+    private void addButtonOpenClose(AbstractLayout layout) {
+        Button button = new Button("+");
+        button.addClickListener(e -> {
+            if (button.getCaption().equals("+")) {
+                button.setCaption("-");
+                addButtons(layout);
+            } else {
+                button.setCaption("+");
+                layout.removeAllComponents();
+                layout.addComponent(button);
+            }
+        });
+        layout.addComponent(button);
+    }
 
-		return layout;
-	}
+    private void addButtons(AbstractLayout layout) {
+        for (String pageName : getMapPageAndDescription().keySet()) {
+            Button button = new Button(getMapPageAndDescription().get(pageName));
+            button.addClickListener(e -> {
+                // getUi().getPage().setLocation("/" +
+                // EwfCommonConstants.APPLICATION_NAME + "/" + pageName +
+                // "/");
+                try {
+                    getClient().loadPage(getUi(), pageName);
+                } catch (EpeAppException ex) {
+                }
+            });
+            layout.addComponent(button);
+            // layout.setComponentAlignment(button, Alignment.MIDDLE_LEFT);
+        }
+    }
 
 }
