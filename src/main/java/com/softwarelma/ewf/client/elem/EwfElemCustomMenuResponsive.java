fa52e0958606efff6a76b40b584a002a0666d520
softwarelma <softwarelma@pgmail.com>
2018-01-25 22:23:31 +0100

    RESPONSIVE MENU

--- src/main/java/com/softwarelma/ewf/client/elem/EwfElemCustomMenuResponsive.java
+++ src/main/java/com/softwarelma/ewf/client/elem/EwfElemCustomMenuResponsive.java
@@ -4,77 +4,88 @@ import com.softwarelma.epe.p1.app.EpeAppException;
 import com.softwarelma.epe.p1.app.EpeAppUtils;
 import com.softwarelma.ewf.client.EwfClient;
 import com.softwarelma.ewf.client.cont.EwfContentBean;
+import com.vaadin.server.Responsive;
 import com.vaadin.ui.AbstractLayout;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 public class EwfElemCustomMenuResponsive extends EwfElemAbstract {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public EwfElemCustomMenuResponsive(EwfClient client, UI ui, EwfElemBean elemBean, EwfContentBean contentBean)
-            throws EpeAppException {
-        super(client, ui, elemBean, contentBean);
-    }
+	public EwfElemCustomMenuResponsive(EwfClient client, UI ui, EwfElemBean elemBean, EwfContentBean contentBean)
+			throws EpeAppException {
+		super(client, ui, elemBean, contentBean);
+	}
 
-    @Override
-    public Component getComponent() throws EpeAppException {
-        if (super.getComponent() != null) {
-            return super.getComponent();
-        }
+	@Override
+	public Component getComponent() throws EpeAppException {
+		if (super.getComponent() != null) {
+			return super.getComponent();
+		}
 
-        EpeAppUtils.checkNull("mapPageAndDescription", getMapPageAndDescription());
+		EpeAppUtils.checkNull("mapPageAndDescription", getMapPageAndDescription());
+		CssLayout layout = new CssLayout();
+		layout.setId("EwfElemCustomMenuResponsive");
+		layout.setWidth("100%");
+		layout.addStyleName("ewfToggleDisplay");
+		Responsive.makeResponsive(layout);
 
-        // TODO
-        // 1. make a vert lay with both: hor and vert menu
-        // 2. add toggle classes
-        // 3. set scss ewf classes
-        // 4. add menu main button for vert
+		HorizontalLayout layoutHor = new HorizontalLayout();
+		layoutHor.setId("EwfElemCustomMenuResponsive:horizontal");
+		// layoutHor.setWidth("100%");
+		layoutHor.addStyleName("ewfToggleDisplayBig");
+		VerticalLayout layoutVert = new VerticalLayout();
+		layoutVert.setId("EwfElemCustomMenuResponsive:vertical");
+		layoutVert.addStyleName("ewfToggleDisplaySmall");
+		this.addButtons(layoutHor);
+		this.addButtonOpenClose(layoutVert);
 
-        VerticalLayout layout = new VerticalLayout();
-        // layout.setWidth("100%");
-        HorizontalLayout layoutHor = new HorizontalLayout();
-        VerticalLayout layoutVert = new VerticalLayout();
-        this.addButtons(layoutHor);
-        this.addButtonOpenClose(layoutVert);
-        layout.addComponent(layoutHor);
-        layout.addComponent(layoutVert);
-        return layout;
-    }
+		HorizontalLayout layoutHorExternal = new HorizontalLayout();
+		layoutHorExternal.setWidth("100%");
+		layoutHorExternal.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		layoutHorExternal.addComponent(layoutHor);
 
-    private void addButtonOpenClose(AbstractLayout layout) {
-        Button button = new Button("+");
-        button.addClickListener(e -> {
-            if (button.getCaption().equals("+")) {
-                button.setCaption("-");
-                addButtons(layout);
-            } else {
-                button.setCaption("+");
-                layout.removeAllComponents();
-                layout.addComponent(button);
-            }
-        });
-        layout.addComponent(button);
-    }
+		layout.addComponent(layoutHorExternal);
+		layout.addComponent(layoutVert);
+		return layout;
+	}
 
-    private void addButtons(AbstractLayout layout) {
-        for (String pageName : getMapPageAndDescription().keySet()) {
-            Button button = new Button(getMapPageAndDescription().get(pageName));
-            button.addClickListener(e -> {
-                // getUi().getPage().setLocation("/" +
-                // EwfCommonConstants.APPLICATION_NAME + "/" + pageName +
-                // "/");
-                try {
-                    getClient().loadPage(getUi(), pageName);
-                } catch (EpeAppException ex) {
-                }
-            });
-            layout.addComponent(button);
-            // layout.setComponentAlignment(button, Alignment.MIDDLE_LEFT);
-        }
-    }
+	private void addButtonOpenClose(AbstractLayout layout) {
+		Button button = new Button("+");
+		button.addClickListener(e -> {
+			if (button.getCaption().equals("+")) {
+				button.setCaption("-");
+				addButtons(layout);
+			} else {
+				button.setCaption("+");
+				layout.removeAllComponents();
+				layout.addComponent(button);
+			}
+		});
+		layout.addComponent(button);
+	}
+
+	private void addButtons(AbstractLayout layout) {
+		for (String pageName : getMapPageAndDescription().keySet()) {
+			Button button = new Button(getMapPageAndDescription().get(pageName));
+			button.addClickListener(e -> {
+				// getUi().getPage().setLocation("/" +
+				// EwfCommonConstants.APPLICATION_NAME + "/" + pageName +
+				// "/");
+				try {
+					getClient().loadPage(getUi(), pageName);
+				} catch (EpeAppException ex) {
+				}
+			});
+			layout.addComponent(button);
+			// layout.setComponentAlignment(button, Alignment.MIDDLE_LEFT);
+		}
+	}
 
 }
