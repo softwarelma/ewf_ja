cb236d3d2f967945cba73438c9063ea56bb96f99
softwarelma <softwarelma@pgmail.com>
2018-02-20 22:39:55 +0100

    GRID 5 - FILE UPLOAD

--- src/main/java/com/softwarelma/ewf/client/EwfClientFake.java
+++ src/main/java/com/softwarelma/ewf/client/EwfClientFake.java
@@ -1,14 +1,25 @@
 package com.softwarelma.ewf.client;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.time.LocalDate;
 
+import com.vaadin.server.FileResource;
+import com.vaadin.server.Page;
 import com.vaadin.ui.AbstractLayout;
 import com.vaadin.ui.AbstractOrderedLayout;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.DateField;
+import com.vaadin.ui.Embedded;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.Grid.SelectionMode;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.Upload;
+import com.vaadin.ui.Upload.Receiver;
+import com.vaadin.ui.Upload.SucceededEvent;
+import com.vaadin.ui.Upload.SucceededListener;
 
 public class EwfClientFake {
 
@@ -45,6 +56,55 @@ public class EwfClientFake {
         // grid.setSelectionMode(SelectionMode.SINGLE);
 
         // grid.listener
+
+        ////////////////////////////////////////////////
+
+        // Show uploaded file in this placeholder
+        final Embedded image = new Embedded("");// "Uploaded Image"
+//        image.setWidth("100px");
+        image.setHeight("100px");
+        image.setVisible(false);
+
+        // Implement both receiver that saves upload in a file and
+        // listener for successful upload
+        class ImageUploader implements Receiver, SucceededListener {
+            public File file;
+
+            public OutputStream receiveUpload(String filename, String mimeType) {
+                // Create upload stream
+                FileOutputStream fos = null; // Stream to write to
+                try {
+                    // Open the file for writing.
+                    file = new File("/home/ellison/Downloads/ewf/" + filename);
+                    fos = new FileOutputStream(file);
+                } catch (final java.io.FileNotFoundException e) {
+                    new Notification("Could not open file<br/>", e.getMessage(), Notification.Type.ERROR_MESSAGE)
+                            .show(Page.getCurrent());
+                    return null;
+                }
+                return fos; // Return the output stream to write to
+            }
+
+            public void uploadSucceeded(SucceededEvent event) {
+                // Show the uploaded file in the image viewer
+                image.setVisible(true);
+                image.setSource(new FileResource(file));
+            }
+        }
+
+        ImageUploader receiver = new ImageUploader();
+
+        // Create the upload with a caption and set receiver later
+        Upload upload = new Upload("", receiver);// "Upload Image Here"
+        upload.setButtonCaption("Start Upload");
+        upload.addSucceededListener(receiver);
+
+        // Put the components in a panel
+        // Panel panel = new Panel("Cool Image Storage");
+        // Layout panelContent = new VerticalLayout();
+        // panelContent.addComponents(upload, image);
+        // panel.setContent(panelContent);
+        layout.addComponents(upload, image);
     }
 
     // TODO
